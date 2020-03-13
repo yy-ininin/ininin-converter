f738e5399c61d9a2289a06dfd7d6c0ff6c59a851
xiaojian <xiaojian00007@163.com>
2020-03-13 20:25:49 +0800

    123

--- index.js
+++ index.js
@@ -3,11 +3,56 @@
  * @summary 转换处理类
  * @namespace ininin-converter
  * @author woo@ininin.com
- * @version 1.1
+ * @version 1.1.0
  * @since 2016/2/22
  * @constructor
+ *-------------------------
+ * @author shlijian@ininin.com
+ * @version 1.1.1
+ * @since 2020/3/13
+ * @description 添加首字母大写 / 首页母小写 / 大小写转换 / 全部大写 / 全部小写
  */
-module.exports =  {
+
+module.exports = {
+    /** 首字母大写 / 首页母小写 / 大小写转换 / 全部大写 / 全部小写
+     * @param  {str} 必选，字符串
+     * @param  {type} type:  1:首字母大写  2：首页母小写  3：大小写转换  4：全部大写  5：全部小写
+     * @return {String} 转换后的字符串
+     */
+    changeCase(str, type) {
+      type = type || 4;
+      switch (type) {
+        case 1:
+          return str.replace(/\b\w+\b/g, function(word) {
+            return (
+              word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase()
+            );
+          });
+        case 2:
+          return str.replace(/\b\w+\b/g, function(word) {
+            return (
+              word.substring(0, 1).toLowerCase() + word.substring(1).toUpperCase()
+            );
+          });
+        case 3:
+          return str
+            .split("")
+            .map(function(word) {
+              if (/[a-z]/.test(word)) {
+                return word.toUpperCase();
+              } else {
+                return word.toLowerCase();
+              }
+            })
+            .join("");
+        case 4:
+          return str.toUpperCase();
+        case 5:
+          return str.toLowerCase();
+        default:
+          return str;
+      }
+    },
     /**
      * 字符串或数字转半角
      * @method decodeHashString
@@ -15,18 +60,18 @@ module.exports =  {
      * @returns {String} 转换后的字符串
      */
     toDBC(str) {
-        str = String(str == null ? "" : str);
-        let result = "";
-        for (let i = 0; i < str.length; i++) {
-            if (str.charCodeAt(i) == 12288) {
-                result += String.fromCharCode(str.charCodeAt(i) - 12256);
-            } else if (str.charCodeAt(i) > 65280 && str.charCodeAt(i) < 65375) {
-                result += String.fromCharCode(str.charCodeAt(i) - 65248);
-            } else {
-                result += String.fromCharCode(str.charCodeAt(i));
-            }
+      str = String(str == null ? "" : str);
+      let result = "";
+      for (let i = 0; i < str.length; i++) {
+        if (str.charCodeAt(i) == 12288) {
+          result += String.fromCharCode(str.charCodeAt(i) - 12256);
+        } else if (str.charCodeAt(i) > 65280 && str.charCodeAt(i) < 65375) {
+          result += String.fromCharCode(str.charCodeAt(i) - 65248);
+        } else {
+          result += String.fromCharCode(str.charCodeAt(i));
         }
-        return result;
+      }
+      return result;
     },
     /**
      * 字符串或数字转全角
@@ -35,18 +80,18 @@ module.exports =  {
      * @returns {String} 转换后的字符串
      */
     toSBC(str) {
-        str = String(str == null ? "" : str);
-        let result = "";
-        for (let i = 0; i < str.length; i++) {
-            if (str.charCodeAt(i) == 32) {
-                result += String.fromCharCode(12288);
-            } else if (str.charCodeAt(i) < 127) {
-                result += String.fromCharCode(str.charCodeAt(i) + 65248);
-            } else {
-                result += String.fromCharCode(str.charCodeAt(i));
-            }
+      str = String(str == null ? "" : str);
+      let result = "";
+      for (let i = 0; i < str.length; i++) {
+        if (str.charCodeAt(i) == 32) {
+          result += String.fromCharCode(12288);
+        } else if (str.charCodeAt(i) < 127) {
+          result += String.fromCharCode(str.charCodeAt(i) + 65248);
+        } else {
+          result += String.fromCharCode(str.charCodeAt(i));
         }
-        return result;
+      }
+      return result;
     },
     /**
      * 解析键值对字符串为Hash对象
@@ -57,29 +102,29 @@ module.exports =  {
      * @returns {Object} Hash对象
      */
     decodeHashString(str, sign, flag) {
-        let arr = str ? str.split(sign == null ? "&" : sign) : [];
-        let hashs = {};
-        let reg = new RegExp(
-            "(^|" +
-            (sign || "&") +
-            ")([^" +
-            (flag || "=") +
-            "]*)" +
-            (flag || "=") +
-            "([^" +
-            (sign || "&") +
-            "]*)(" +
-            (sign || "&") +
-            "|$)",
-            "i"
-        );
-        for (let i = 0, l = arr.length; i < l; i++) {
-            let parts = arr[i].match(reg) || [];
-            if (parts[2] != null && parts[2] !== "") {
-                hashs[parts[2]] = decodeURIComponent(parts[3] == null ? "" : parts[3]);
-            }
+      let arr = str ? str.split(sign == null ? "&" : sign) : [];
+      let hashs = {};
+      let reg = new RegExp(
+        "(^|" +
+          (sign || "&") +
+          ")([^" +
+          (flag || "=") +
+          "]*)" +
+          (flag || "=") +
+          "([^" +
+          (sign || "&") +
+          "]*)(" +
+          (sign || "&") +
+          "|$)",
+        "i"
+      );
+      for (let i = 0, l = arr.length; i < l; i++) {
+        let parts = arr[i].match(reg) || [];
+        if (parts[2] != null && parts[2] !== "") {
+          hashs[parts[2]] = decodeURIComponent(parts[3] == null ? "" : parts[3]);
         }
-        return hashs;
+      }
+      return hashs;
     },
     /**
      * 编码Hash对象为键值对字符串
@@ -91,16 +136,19 @@ module.exports =  {
      * @returns {String} 键值对字符串
      */
     encodeHashString(hashs, sign, flag) {
-        let arr = [];
-        for (let key in hashs) {
-            if (hashs.hasOwnProperty && hashs.hasOwnProperty(key)) {
-                arr.push(
-                    key +
-                    (flag == null ? "=" : flag) +
-                    encodeURIComponent(decodeURIComponent(hashs[key] == null ? "" : hashs[key]))
-                );
-            }
+      let arr = [];
+      for (let key in hashs) {
+        if (hashs.hasOwnProperty && hashs.hasOwnProperty(key)) {
+          arr.push(
+            key +
+              (flag == null ? "=" : flag) +
+              encodeURIComponent(
+                decodeURIComponent(hashs[key] == null ? "" : hashs[key])
+              )
+          );
         }
-        return arr.join(sign == null ? "&" : sign);
+      }
+      return arr.join(sign == null ? "&" : sign);
     }
-};
+  };
+  
